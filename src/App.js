753420a8491e1e3cdf126b6cd588ffa50e1dd7cf
NgocNguyenC3 <nguyen.dn.hubtech@gmail.com>
2022-11-01 20:42:51 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import { Register } from './components/Register.tsx';
+import { Main } from './components/Main.tsx';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='main'>
-      <Register/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav className="nav">
+          <div className="link-item">
+            <Link to="/main">Main</Link>
+          </div>
+          <div className="link-item">
+            <Link to="/register">Register</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route
+            path="/main"
+            element={<Main />}
+          />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,48 +1,45 @@
 import React from "react";
 import './register.css';
 import {
+
     EyeOutlined,
     EyeInvisibleOutlined,
 } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+
 import axios from "axios";
+
+import 'antd/dist/antd.css';
 
 
 export const Register = () => {
+    const [form] = Form.useForm();
+
     const [registerForm, setRegisterForm] = React.useState({
         email: "",
         password: "",
         confirmPassword: "",
     });
-    const [errorMsg, setErrorMsg] = React.useState<string | undefined>(undefined);
 
-    const handleChangeData = (e: React.FormEvent<HTMLFormElement>) => {
-        const { name, value } = e.target as HTMLInputElement;
-        if (name === "confirmPassword") {
-            if (value !== registerForm.password) {
-                setErrorMsg("Passwords do not match");
-            } else {
-                setErrorMsg(undefined);
-            }
-        }
-        setRegisterForm({ ...registerForm, [name]: value });
-    };
 
-    const handleSubmitData = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmitData = async (e: any) => {
+        setRegisterForm({
+            email: form.getFieldValue("email"),
+            password: form.getFieldValue("password"),
+            confirmPassword: form.getFieldValue("confirmPassword"),
+        })
         e.preventDefault();
-        console.log(1);
-        if (errorMsg || !registerForm.password || !registerForm.confirmPassword) {
-            console.log(2);
+        if (!registerForm.email || !registerForm.password || !registerForm.confirmPassword) {
+            alert("Check all field again")
             return;
         }
 
 
         if (registerForm.password !== registerForm.confirmPassword) {
-            console.log(3);
-            setErrorMsg("Passwords do not match");
+            alert("Password do not match")
             return;
         }
         try {
-            console.log(3);
             await registerApp(registerForm);
             alert("Register successful!")
         } catch (err) {
@@ -51,76 +48,94 @@ export const Register = () => {
     };
     return (
         <div className="screen">
-            <form
+
+            <Form
+                form={form}
                 className="screen"
-                onChange={handleChangeData}
-                onSubmit={handleSubmitData}
             >
                 <h2>Register</h2>
-                <div className="email">
-                    <label htmlFor="email">Email</label>
-                    <div className="sec-2">
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="example@gmail.com"
-                            required
-                        />
+                <Form.Item
+                    className="email"
+                    name={"email"}
+                    rules={[
+                        { required: true },
+                        { type: "email", message: "Please enter valid email" },
+                    ]}
+                >
+                    <Input
+                        className="input"
+                        placeholder="example@gmail.com"
+                        required
+                    />
+                </Form.Item>
+                <Form.Item
+                    className="password"
+                    name={"password"}
+                    rules={[
+                        { required: true },
+                        {
+                            //pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+                            message:
+                                "Required password > 8 characters, >1 number, >1 uppercase letter, >1 lowercase letter, 1 special letter",
+                        },
+                    ]}
+                >
+                    <Input.Password
+                        className="input"
+                        name={"pasword"}
+                        placeholder={"Password"}
+                        iconRender={(visible) =>
+                            visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
+                        }
+                        required
+                    />
+                </Form.Item>
+                <Form.Item
+                    className="password"
+                    name={"confirmPassword"}
+                    rules={[
+                        { required: true },
+                        {
+                            message:
+                                "Please enter a valid password with mininum 8 characters, at least one uppercase letter, one lowercase letter and one number",
+                        },
+                        {
+                            validator: (rule, value) => {
+                                if (value !== form.getFieldValue("password")) {
+                                    return Promise.reject("Passwords do not match");
+                                }
+                            },
+                        },
+                    ]}
+                >
+                    <Input.Password
+                        className="input"
+                        name={"confirmPassword"}
+                        placeholder={"confirm passoword"}
+                        iconRender={(visible) =>
+                            visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
+                        }
+                        required
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <div className="button">
+                        <Button
+                            type="primary"
+                            className="register"
+                            ghost
+                            onClick={handleSubmitData}
+                        >
+                            Register
+                        </Button>
                     </div>
-                </div>
-                <PasswordInput name="password" label="Password" />
-                <PasswordInput
-                    name="confirmPassword"
-                    label="Confirm Password"
-                    errorMsg={errorMsg}
-                />
-                <button className="register">Register</button>
-
-            </form>
+                </Form.Item>
+            </Form>
         </div>
     );
 };
 
-export const PasswordInput = ({
-    name,
-    label,
-    errorMsg,
-}: {
-    name: string;
-    label: string;
-    errorMsg?: string;
-}) => {
-    const [visible, setVisible] = React.useState(false);
-    return (
-        <div className="password">
-            <label htmlFor={name}>{label}</label>
-            <div className="sec-2">
-                <input
-                    type={visible ? "text" : "password"}
-                    name={name}
-                    placeholder="············"
-                    //pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
-                    title="Required password > 8 characters, >1 number, >1 uppercase letter, >1 lowercase letter, 1 special letter"
-                    required
-                />
-                {visible ? (
-                    <EyeOutlined
-                        onClick={() => {
-                            setVisible(false);
-                        }}
-                    />
-                ) : (
-                    <EyeInvisibleOutlined
-                        onClick={() => {
-                            setVisible(true);
-                        }}
-                    />
-                )}
-            </div>
-            {errorMsg && <span className="error">{errorMsg}</span>}
-        </div>
-    );
-};
+
 
 interface RegisterData {
     email: string;
